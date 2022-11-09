182be4876436222d330ff93046dc05fd1563e11b
DA_M1_35 <da_m1_35@Academy-learners-MacBook-Air.local>
2022-11-09 15:12:53 +0200

    projects going

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    ExperienceComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
